ae478413dd98d52e29ead711bbe95baf908aab5f
Johanna Blaha <s2110456004@fhooe.at>
2023-06-19 20:11:13 +0200

    04 - Feinschliff

--- dark_light.js
+++ dark_light.js
@@ -1,16 +1,17 @@
 //JS welches den Darkmode-Button abfragt und je nach Status im HTML-Tag light oder dark ersetzt
 window.addEventListener("DOMContentLoaded", (event) => {
     let btn= document.getElementById('modeToggle');
-
     btn.addEventListener('click', toggle);
 
     function toggle() {
         console.log('Button clicked!');
         if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-            document.documentElement.setAttribute('data-bs-theme','light')
+            document.documentElement.setAttribute('data-bs-theme','light');
+            document.querySelector('.main-pic').src = 'img/monstera_plant_light.png';
         }
         else {
-            document.documentElement.setAttribute('data-bs-theme','dark')
+            document.documentElement.setAttribute('data-bs-theme','dark');
+            document.querySelector('.main-pic').src = 'img/monstera_plant_dark.png';
         }
     }
 });

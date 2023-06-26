abcac8f54b2ade528c71efa478ecd420b8c2d613
Johanna Blaha <s2110456004@fhooe.at>
2023-06-27 01:59:12 +0200

    - Icons Dark-Mode - Cards Bilder anpassen (overflow hidden) - Buttons Hover-Effekt - Accordeon Fokus und Farbgebung - shame.css auflösen - styles.css bereinigen - dark-light.jz an Änderungen anpassen

--- dark_light.js
+++ dark_light.js
@@ -1,17 +1,21 @@
-//JS welches den Darkmode-Button abfragt und je nach Status im HTML-Tag light oder dark ersetzt
+// Query the mode button and replaces light or dark depending on the status in the HTML tag
 window.addEventListener("DOMContentLoaded", (event) => {
-    let btn= document.getElementById('modeToggle');
-    btn.addEventListener('click', toggle);
+    let toggleBtn= document.querySelectorAll('.mode-toggle');
+    for (let btn of toggleBtn) {
+        btn.addEventListener('click', toggle);
+    }
 
     function toggle() {
-        console.log('Button clicked!');
-        if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        if (document.documentElement.getAttribute('data-bs-theme') === 'dark') {
             document.documentElement.setAttribute('data-bs-theme','light');
-            document.querySelector('.main-pic').src = 'img/monstera_plant_light.png';
-        }
-        else {
+            document.querySelector('.main-img').src = 'img/monstera_plant_light.png';
+            document.querySelector('.watering-can-img').src = 'img/watering-can_light.png';
+            document.querySelector('.shovel-img').src = 'img/shovel_light.png';
+        } else {
             document.documentElement.setAttribute('data-bs-theme','dark');
-            document.querySelector('.main-pic').src = 'img/monstera_plant_dark.png';
+            document.querySelector('.main-img').src = 'img/monstera_plant_dark.png';
+            document.querySelector('.watering-can-img').src = 'img/watering-can_dark.png';
+            document.querySelector('.shovel-img').src = 'img/shovel_dark.png';
         }
     }
 });
